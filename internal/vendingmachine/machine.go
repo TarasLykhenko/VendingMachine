90a833a75f7b7ea99bb5db3eba34d0cc009d4ae1
Samuel Hopko <samuel.hopko@outsystems.com>
2021-10-21 14:52:56 +0200

    Accept working

--- internal/vendingmachine/machine.go
+++ internal/vendingmachine/machine.go
@@ -1,5 +1,9 @@
 package vendingmachine
 
+import (
+	"fmt"
+)
+
 //Coin .
 type Coin interface {
 	Mass() float32
@@ -11,6 +15,8 @@ type Coin interface {
 type VendingMachine struct {
 }
 
+var money float32 = 0
+
 // NewVendingMachine .
 func NewVendingMachine() *VendingMachine {
 	return &VendingMachine{}
@@ -18,7 +24,18 @@ func NewVendingMachine() *VendingMachine {
 
 // AcceptCoin .
 func (v *VendingMachine) AcceptCoin(coin Coin) {
-
+	if coin.Mass() == 2.5 && coin.Diameter() == 19.05 && coin.Thickness() == 1.52 {
+		fmt.Printf("I DO NOT ACCEPT PENNIES\n")
+	}
+	if coin.Mass() == 5 && coin.Diameter() == 21.21 && coin.Thickness() == 1.95 {
+		money += 0.05
+	}
+	if coin.Mass() == 2.26 && coin.Diameter() == 17.91 && coin.Thickness() == 1.35 {
+		money += 0.1
+	}
+	if coin.Mass() == 6.25 && coin.Diameter() == 24.26 && coin.Thickness() == 1.75 {
+		money += 0.25
+	}
 }
 
 // SelectProduct .
@@ -38,7 +55,10 @@ func (v *VendingMachine) ReturnCoins() {
 
 // Display .
 func (v *VendingMachine) Display() string {
-	return "ERROR"
+	if money == 0 {
+		return "INSERT COIN\n"
+	}
+	return fmt.Sprintf("CURRENT STATUS: $%f", money)
 }
 
 // RestockProducts .
