e394ab0c35074c7e46f10ccf662b5ceef83046b3
Pedro Carmo <pedrocarmo@PedroCakMacBook.localdomain>
2021-10-21 15:12:19 +0100

    Starting the product selector

--- internal/vendingmachine/machine.go
+++ internal/vendingmachine/machine.go
@@ -1,5 +1,12 @@
 package vendingmachine
 
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/TarasLykhenko/vending-machine-kata/internal/products"
+)
+
 //Coin .
 type Coin interface {
 	Mass() float32
@@ -38,7 +45,25 @@ func (v *VendingMachine) ReturnCoins() {
 
 // Display .
 func (v *VendingMachine) Display() string {
-	return "ERROR"
+	var output string
+	list := []products.IProduct{}
+
+	list = append(list, products.NewCola())
+	list = append(list, products.NewChips("Sea Salt"))
+	list = append(list, products.NewChips("Bacon"))
+	list = append(list, products.NewCandy("Blue"))
+	list = append(list, products.NewCandy("Red"))
+
+	output = "INSERT COIN:\n"
+	for i, product := range list {
+		output += strconv.Itoa(i) + ". " + product.Name() + "\n"
+		output += "  " + product.ExtraInformation() + "\n"
+		output += "  Price: " + fmt.Sprintf("%.2f", product.Price()) + "€\n"
+
+		output += "\n"
+	}
+
+	return output
 }
 
 // RestockProducts .
